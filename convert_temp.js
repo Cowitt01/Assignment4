2a36376eadc4a15438b1a8d6a6f92ac772d0b5ae
Cory Witt <cpwitt@mail.mccneb.edu>
2025-06-30 23:21:46 -0500

    Update

--- convert_temp.js
+++ convert_temp.js
@@ -18,18 +18,17 @@ const $ = selector => document.querySelector(selector);
 const calculateCelsius = temp => (temp-32) * 5/9;             //celcius to farenheit calculation
 const calculateFahrenheit = temp => temp * 9/5 + 32;          //farenheit to celcius calcuation
 const calculateMeters = meters => meters / 3.2808;            //feet to meters calcuations
-const calculateFeet = feet => meters * 3.2808;                //meters to feet calcuation
+const calculateFeet = feet => feet * 3.2808;                  //meters to feet calcuation
 
 const toggleDisplay = (label1Text, label2Text) => {           //Toggles the values for label 1 and label 2
     document.getElementById("label_1").textContent = label1Text;
     document.getElementById("label_2").textContent = label2Text;
     
     document.getElementById('value_computed').textContent = "";
-
-
-    // select text in degrees textbox
-
     
+
+    // selec
+
 }
 
 /****************************
@@ -39,46 +38,46 @@ const performConversion = () => {   /* This function will check with conversion 
                                        selected, check to make sure that the value entered by the 
                                        end user is numeric, and then perform the appropriate calcuation*/
    
-   let conversionType = "";
+ const selectedRadio = $('input[name="conversion_type"]:checked').id;
 
-   const selectedConversionRadio = $('input[name="conversion_type"]:checked')
+ let conversionType = "";
 
-   if (selectedConversionRadio.id = "to_celsius") {
-      conversionType = "celsius"
-   }else if (selectedConversionRadio.id = "to_fahrenheit")  {
-      conversionType = "fahrenheit"    
-   }else if (selectedConversionRadio.id = "to_meters")  {
-      conversionType = "meters"
-   }else if (selectedConversionRadio.id = "to_feet")  {
-      conversionType = "feet"
-   }
+ if (selectedRadio == "to_celsius") {      
+      conversionType = "celsius";                             //if "to celcius" conversion type is checked
+   }else if (selectedRadio == "to_fahrenheit")  {
+      conversionType = "fahrenheit";                          //if "to farenheit" conversion type is checked
+   }else if (selectedRadio == "to_meters")  {
+      conversionType = "meters";                              //if "to meters" conversion type is checked
+   }else if (selectedRadio == "to_feet")  {
+      conversionType = "feet" ;                               //if "to feet" conversion type is checked
+}
 
-   const inputValue = parseFloat($("#value_entered").value);
+const inputValue = parseFloat($("#value_entered").value);
    
-   if (isNaN(inputValue)) {        //If input value is not numeric, display error message to user.
-       $("#message").textContent = "You must enter a valid number for " + conversionType + " conversions." ;
-       $("#value_entered").textContent = "";
+if (isNaN(inputValue)) {        //If input value is not numeric, display error message to user.
+     $("#message").textContent = "You must enter a valid number for " + conversionType + " conversions." ;
+     $("#value_entered").textContent = "";
 
-   }else{
-       $("#message").textContent = "";   //input value is numeric, perform appropriate calculation routine
+}else{
+      $("#message").textContent = "";   //input value is numeric, perform appropriate calculation routine
 
-       if (conversionType = "celcius") {
+      if (conversionType == "celsius") {
           $('#value_computed').value = (calculateCelsius(inputValue)).toFixed(2);     //Call the calculateCelsius function
-       }else if (conversionType = "fahrenheit") {
+       }else if (conversionType == "fahrenheit") {
           $('#value_computed').value = (calculateFahrenheit(inputValue)).toFixed(2);  //Call the calculateFarenheit function
-       }else if (conversionType = "meters") {
+       }else if (conversionType == "meters") {
           $('#value_computed').value = (calculateMeters(inputValue)).toFixed(2);      //Call the calculate Meters function
-       }else if (conversionType ="feet") {
+       }else if (conversionType == "feet") {
           $('#value_computed').value = (calculateFeet(inputValue)).toFixed(2);        //Call the calculate Feet function  
        }
    }
 }
 
-const toggleToCelsius = () => toggleDisplay("Enter F degrees:", "Degrees Celsius:");
-const toggleToFahrenheit = () => toggleDisplay("Enter C degrees:", "Degrees Fahrenheit:");
+const toggleToCelsius = () => toggleDisplay("Enter F degrees:", "Degrees Celsius:");         //label change for celcius conversion
+const toggleToFahrenheit = () => toggleDisplay("Enter C degrees:", "Degrees Fahrenheit:");   //label change for farenheit conversion
 
-const toggleToMeters = () => toggleDisplay("Enter feet:", "Meters:");
-const toggleToFeet = () => toggleDisplay("Enter meters:", "Feet:");
+const toggleToMeters = () => toggleDisplay("Enter feet:", "Meters:");                        //label change for feet conversion
+const toggleToFeet = () => toggleDisplay("Enter meters:", "Feet:");                          //label changefor meters conversion
 
 /****************************************************
 *              Event listeners                      *
