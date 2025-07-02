1cdca79a9a2259d46b0fb3c40c5cfbf11b90779b
Cory Witt <cpwitt@mail.mccneb.edu>
2025-07-01 20:10:42 -0500

    Update

--- convert_temp.js
+++ convert_temp.js
@@ -23,12 +23,10 @@ const calculateFeet = feet => feet * 3.2808;                  //meters to feet c
 const toggleDisplay = (label1Text, label2Text) => {           //Toggles the values for label 1 and label 2
     document.getElementById("label_1").textContent = label1Text;
     document.getElementById("label_2").textContent = label2Text;
-    
-    document.getElementById('value_computed').textContent = "";
-    
-
-    // selec
-
+    $("#value_computed").value= "";
+    $("#value_entered").value= "";
+    $("#value_entered").focus();
+ 
 }
 
 /****************************
@@ -39,6 +37,7 @@ const performConversion = () => {   /* This function will check with conversion 
                                        end user is numeric, and then perform the appropriate calcuation*/
    
  const selectedRadio = $('input[name="conversion_type"]:checked').id;
+ console.log("Selected radio button is  " + selectedRadio);
 
  let conversionType = "";
 
@@ -51,26 +50,34 @@ const performConversion = () => {   /* This function will check with conversion 
    }else if (selectedRadio == "to_feet")  {
       conversionType = "feet" ;                               //if "to feet" conversion type is checked
 }
-
+ 
 const inputValue = parseFloat($("#value_entered").value);
+console.log("Text input value is  " + inputValue);
    
 if (isNaN(inputValue)) {        //If input value is not numeric, display error message to user.
      $("#message").textContent = "You must enter a valid number for " + conversionType + " conversions." ;
-     $("#value_entered").textContent = "";
+     $("#value_entered").value= "";
+     $("#value_entered").focus();
 
 }else{
       $("#message").textContent = "";   //input value is numeric, perform appropriate calculation routine
 
       if (conversionType == "celsius") {
           $('#value_computed').value = (calculateCelsius(inputValue)).toFixed(2);     //Call the calculateCelsius function
-       }else if (conversionType == "fahrenheit") {
-          $('#value_computed').value = (calculateFahrenheit(inputValue)).toFixed(2);  //Call the calculateFarenheit function
-       }else if (conversionType == "meters") {
-          $('#value_computed').value = (calculateMeters(inputValue)).toFixed(2);      //Call the calculate Meters function
-       }else if (conversionType == "feet") {
-          $('#value_computed').value = (calculateFeet(inputValue)).toFixed(2);        //Call the calculate Feet function  
-       }
+          console.log("Calculated Celcius value " +  $('#value_computed').value);
+         }else if (conversionType == "fahrenheit") {
+                  $('#value_computed').value = (calculateFahrenheit(inputValue)).toFixed(2);  //Call the calculateFarenheit function
+                   console.log("Calculated Farenheit value " +  $('#value_computed').value);
+         }else if (conversionType == "meters") {
+                  $('#value_computed').value = (calculateMeters(inputValue)).toFixed(2);      //Call the calculate Meters function
+                  console.log("Calculated Meters value " +  $('#value_computed').value);
+         }else if (conversionType == "feet") {
+                  $('#value_computed').value = (calculateFeet(inputValue)).toFixed(2);        //Call the calculate Feet function  
+                  console.log("Calculated Feet value " +  $('#value_computed').value);
+         }
    }
+    $("#value_entered").focus();
+    $("#value_entered").select();
 }
 
 const toggleToCelsius = () => toggleDisplay("Enter F degrees:", "Degrees Celsius:");         //label change for celcius conversion
@@ -84,10 +91,10 @@ const toggleToFeet = () => toggleDisplay("Enter meters:", "Feet:");             
 *****************************************************/
 document.addEventListener("DOMContentLoaded", () => {
 	$("#convert").addEventListener("click", performConversion);
-    $("#to_celsius").addEventListener("click", toggleToCelsius);
-    $("#to_fahrenheit").addEventListener("click", toggleToFahrenheit);
+   $("#to_celsius").addEventListener("click", toggleToCelsius);
+   $("#to_fahrenheit").addEventListener("click", toggleToFahrenheit);
 	$("#to_meters").addEventListener("click", toggleToMeters);
-    $("#to_feet").addEventListener("click", toggleToFeet);
+   $("#to_feet").addEventListener("click", toggleToFeet);
 
 	// move focus to the value entered text box.
 	$("#value_entered").focus();
